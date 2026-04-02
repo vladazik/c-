d8512726e89217e31a0d2de43f0a29c5b32bad92
vladazik <vladochka08092008@gmail.com>
2026-04-03 00:21:52 +0500

    для подготовки к тесту заливаю

---
+++ c#)/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c__
+{
+    public struct Figure2D
+    {
+        private double _x;
+        private double _y;
+
+        public Figure2D(double x, double y)
+        {
+
+            _x = x;
+            _y = y;
+        }
+
+        public void Init(double x, double y)
+        {
+            _x = x;
+            _y = y;
+            Class2.Asd(this);
+        }
+    }
+        // область видимоти блочная 
+}
---
+++ c#)/Class2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c__
+{
+    public class Class2
+    {
+
+        private string _position;//принадлежат 
+        public static decimal salary;
+        public const string COMPANY_NAME = "Ростелеком";
+        public readonly string departament;//можно проинициализировать  при создании объекта
+
+
+
+        //private string _name;
+    }
+        public Class2(string position, string departament)
+        {
+            _position = position;
+            this.departament = departament;//неявная ссылка на экзепляр этого класса 
+        }
+        public Class2()
+        {
+
+        }
+        public static int Increment(int a) {
+
+
+        }
+
+    }
+}
